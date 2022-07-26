3402be1ba769799ec2e5a1013c3c86ee7b24b8bc
Sammy Chang <sammychang2185@gmail.com>
2022-07-25 20:01:23 -0700

    Update styling and how terminal is toggled

--- components/terminal.tsx
+++ components/terminal.tsx
@@ -1,9 +1,9 @@
-import { useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { Terminal } from 'xterm';
 import { FitAddon } from 'xterm-addon-fit';
 
 export default function DynamicTerminal() {
-  const [isTerminalOpen, toggleTerminal] = useState(false);
+  const [isTerminalOpen, setIsTerminalOpen] = useState(false);
   const [history, setHistory] = useState<string[]>([]);
   const terminalRef = useRef<HTMLDivElement>(null);
   let currentLine = 1;
@@ -14,55 +14,78 @@ export default function DynamicTerminal() {
     cursorBlink: true,
     cursorStyle: 'bar',
   });
-
-  const openTerminal = (toggle: boolean) => {
-    if (!toggle) {
-      terminalRef.current?.replaceChildren();
-    } else {
-      const fitAddon = new FitAddon();
-      term.loadAddon(fitAddon);
-      term.open(terminalRef.current!);
-      fitAddon.fit();
-      term.write('\x1B[92mgit@sandbox\x1B[0m $ ');
-      term.onKey((e) => {
-        currentLineContent += e.key;
-        term.write(e.key);
-      });
-      term.attachCustomKeyEventHandler((e) => {
-        switch (e.key) {
-          case 'Enter':
-            if (e.type === 'keydown') {
-              setHistory(history.concat([currentLineContent]));
-              currentLine += Math.ceil((currentLineContent.length + prefix.replace(/[^\x00-\x7F]/g, '').length) / term.cols) + 1;
-              currentLineContent = '';
-              term.write('\r\n\r\n\x1B[92mgit@sandbox\x1B[0m $ ');
-            }
-            return false;
-          case 'Backspace':
-            if (e.type === 'keydown') {
-              term.write(`\x1B[${currentLine}H`);
-              if (currentLineContent) {
-                currentLineContent = currentLineContent.slice(0, currentLineContent.length - 1);
-                term.write('\x1B[J\r');
-                term.write(`\x1B[92mgit@sandbox\x1B[0m $ ${currentLineContent}`);
-              } else {
-                term.write('\x1B[J\r');
-                term.write('\x1B[92mgit@sandbox\x1B[0m $ ');
-              }
-            }
-            return false;
-          default:
-            return true;
+  const fitAddon = new FitAddon();
+  term.loadAddon(fitAddon);
+  fitAddon.fit();
+  term.write('\x1B[92mgit@sandbox\x1B[0m $ ');
+  term.onKey((e) => {
+    currentLineContent += e.key;
+    term.write(e.key);
+  });
+  term.attachCustomKeyEventHandler((e) => {
+    switch (e.key) {
+      case 'Enter':
+        if (e.type === 'keydown') {
+          execCommand(currentLineContent);
         }
-      });
+        return false;
+      case 'Backspace':
+        if (e.type === 'keydown') {
+          term.write(`\x1B[${currentLine}H`);
+          if (currentLineContent) {
+            currentLineContent = currentLineContent.slice(0, currentLineContent.length - 1);
+            term.write('\x1B[J\r');
+            term.write(`\x1B[92mgit@sandbox\x1B[0m $ ${currentLineContent}`);
+          } else {
+            term.write('\x1B[J\r');
+            term.write('\x1B[92mgit@sandbox\x1B[0m $ ');
+          }
+        }
+        return false;
+      default:
+        return true;
     }
-    toggleTerminal(toggle);
+  });
+
+  useEffect(() => {
+    if (!isTerminalOpen) {
+      term.open(terminalRef.current!);
+      setIsTerminalOpen(true);
+    }
+  }, []);
+
+  const execCommand = (cmd: string) => {
+    setHistory(history.concat([cmd]));
+
+    const parsedCmd = cmd.split(' ');
+    switch (parsedCmd[0]) {
+      case 'help':
+        term.write('\
+        \r\nWelcome to Git Sandbox! Try some of the commands below.\
+        \r\n\
+        \r\nhelp   Prints this help message\
+        \r\ngit    Simulate git commands\
+        ');
+        currentLine += 4;
+        break;
+      default:
+        term.write(`\r\n${parsedCmd[0]}: command not found`);
+        currentLine += 2;
+    }
+
+    currentLineContent = '';
+    term.write('\r\n\r\n\x1B[92mgit@sandbox\x1B[0m $ ');
+    currentLine += Math.ceil((cmd.length + prefix.replace(/[^\x00-\x7F]/g, '').length) / term.cols) + 1;
+  };
+
+  const toggleTerminal = () => {
+    setIsTerminalOpen(!isTerminalOpen);
   };
 
   return (
     <div>
-      <div id='terminal' ref={terminalRef} />
-      <button onClick={() => openTerminal(!isTerminalOpen)}>Toggle Terminal</button>
+      <div id='terminal' ref={terminalRef} style={{ display: `${isTerminalOpen ? 'block' : 'none'}` }} />
+      <button onClick={() => toggleTerminal()}>Toggle Terminal</button>
     </div>
   );
 }
