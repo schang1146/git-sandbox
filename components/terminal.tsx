20b20e818c4104947c9d8e95bd892fe1ba005b66
schang1146 <sammychang2185@gmail.com>
2022-07-30 13:55:41 -0700

    Fix fitting issues

--- components/terminal.tsx
+++ components/terminal.tsx
@@ -3,7 +3,7 @@ import { Terminal } from 'xterm';
 import { FitAddon } from 'xterm-addon-fit';
 
 export default function DynamicTerminal() {
-  const [isTerminalOpen, setIsTerminalOpen] = useState(false);
+  const [isTerminalOpen, setIsTerminalOpen] = useState(true);
   const [history, setHistory] = useState<string[]>([]);
   const terminalRef = useRef<HTMLDivElement>(null);
   let currentLine = 1;
@@ -16,40 +16,37 @@ export default function DynamicTerminal() {
   });
   const fitAddon = new FitAddon();
   term.loadAddon(fitAddon);
-  fitAddon.fit();
-  term.write('\x1B[92mgit@sandbox\x1B[0m $ ');
-  term.onKey((e) => {
-    currentLineContent += e.key;
-    term.write(e.key);
-  });
-  term.attachCustomKeyEventHandler((e) => {
-    switch (e.key) {
-      case 'Enter':
-        if (e.type === 'keydown') {
-          execCommand(currentLineContent);
-        }
-        return false;
-      case 'Backspace':
-        if (e.type === 'keydown') {
-          term.write(`\x1B[${currentLine}H`); // moves to beginning of currentLine
-          term.write('\x1B[J\r'); // erases everything to the right
-          if (currentLineContent) {
-            currentLineContent = currentLineContent.slice(0, currentLineContent.length - 1);
-          }
-          term.write(`\x1B[92mgit@sandbox\x1B[0m $ ${currentLineContent}`);
-        }
-        return false;
-      default:
-        return true;
-    }
-  });
 
   useEffect(() => {
-    if (!isTerminalOpen) {
-      term.open(terminalRef.current!);
-      setIsTerminalOpen(true);
-    }
-  }, []);
+    term.open(terminalRef.current!);
+    fitAddon.fit();
+    term.onKey((e) => {
+      currentLineContent += e.key;
+      term.write(e.key);
+    });
+    term.attachCustomKeyEventHandler((e) => {
+      switch (e.key) {
+        case 'Enter':
+          if (e.type === 'keydown') {
+            execCommand(currentLineContent);
+          }
+          return false;
+        case 'Backspace':
+          if (e.type === 'keydown') {
+            term.write(`\x1B[${currentLine}H`); // moves to beginning of currentLine
+            term.write('\x1B[J\r'); // erases everything to the right
+            if (currentLineContent) {
+              currentLineContent = currentLineContent.slice(0, currentLineContent.length - 1);
+            }
+            term.write(`${prefix}${currentLineContent}`);
+          }
+          return false;
+        default:
+          return true;
+      }
+    });
+    term.write(prefix);
+  }, [terminalRef]);
 
   const execCommand = (cmd: string) => {
     setHistory(history.concat([cmd]));
@@ -71,7 +68,7 @@ export default function DynamicTerminal() {
     }
 
     currentLineContent = '';
-    term.write('\r\n\r\n\x1B[92mgit@sandbox\x1B[0m $ ');
+    term.write(`\r\n\r\n${prefix}`);
     currentLine += Math.ceil((cmd.length + prefix.replace(/[^\x00-\x7F]/g, '').length) / term.cols) + 1;
   };
 
@@ -80,9 +77,9 @@ export default function DynamicTerminal() {
   };
 
   return (
-    <div>
+    <div style={{ width: '900px', height: '600px' }}>
       <div id='terminal' ref={terminalRef} style={{ display: `${isTerminalOpen ? 'block' : 'none'}` }} />
-      <button onClick={() => toggleTerminal()}>Toggle Terminal</button>
+      {/* <button onClick={() => toggleTerminal()}>Toggle Terminal</button> */}
     </div>
   );
 }
