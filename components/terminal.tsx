99cbc83d3c97cb430133458546cb65859b486d6b
Sammy Chang <sammychang2185@gmail.com>
2022-09-22 16:58:53 -0700

    XTERM initial research and POC (#2)
    * Add Xterm and add functional typing and deleting
    
    * Use useRef to get element to attach terminal to
    
    * Fit terminal typing to container size
    
    * Enforce only having 1 terminal open at a time
    
    * Rename Home to index
    
    * Update styling and how terminal is toggled
    
    * Fix line count for unknown command
    
    * Simplify backspace function and add helpful comments
    
    * Fix fitting issues
    
    * Fix unknown cmd line increment
    
    * (WIP) Auto increment line based on action
    
    * Auto increment line based on action
    
    * Prevent multiple terminals on compile
    
    * Handle leading + trailing whitespace
    
    * Add base git help command
    
    * Extract git command to separate file

---
+++ components/terminal.tsx
@@ -0,0 +1,117 @@
+import { useEffect, useRef, useState } from 'react';
+import { Terminal } from 'xterm';
+import { FitAddon } from 'xterm-addon-fit';
+import execGitCmd from '../commands/git';
+
+export default function DynamicTerminal() {
+  const [isTerminalOpen, setIsTerminalOpen] = useState(true);
+  const [history, setHistory] = useState<string[]>([]);
+  const terminalRef = useRef<HTMLDivElement>(null);
+  let currentLine = 1;
+  let currentLineContent = '';
+  const prefix = '\x1B[92mgit@sandbox\x1B[0m $ ';
+  const term = new Terminal({
+    convertEol: true,
+    cursorBlink: true,
+    cursorStyle: 'bar',
+  });
+  const fitAddon = new FitAddon();
+  term.loadAddon(fitAddon);
+
+  useEffect(() => {
+    if (terminalRef.current?.childElementCount) {
+      terminalRef.current?.removeChild(terminalRef.current.firstElementChild!);
+    }
+    console.log('Opening fresh terminal');
+    term.open(terminalRef.current!);
+    console.log(`Before fitting xTerm screen\nContainesr width: ${terminalRef.current?.clientWidth}\nCols: ${term.cols}`);
+    fitAddon.fit();
+    console.log(`After fitting xTerm screen\nContainer width: ${terminalRef.current?.clientWidth}\nCols: ${term.cols}`);
+    term.onKey((e) => {
+      currentLineContent += e.key;
+      term.write(e.key);
+    });
+    term.attachCustomKeyEventHandler((e) => {
+      switch (e.key) {
+        case 'Enter':
+          if (e.type === 'keydown') {
+            execCommand(currentLineContent);
+          }
+          return false;
+        case 'Backspace':
+          if (e.type === 'keydown') {
+            term.write(`\x1B[${currentLine}H`); // moves to beginning of currentLine
+            term.write('\x1B[J\r'); // erases everything to the right
+            if (currentLineContent) {
+              currentLineContent = currentLineContent.slice(0, currentLineContent.length - 1);
+            }
+            term.write(`${prefix}${currentLineContent}`);
+          }
+          return false;
+        default:
+          return true;
+      }
+    });
+    term.write(prefix);
+  }, [terminalRef]);
+
+  const execCommand = (cmd: string) => {
+    setHistory(history.concat([cmd]));
+    currentLine += incrementLines(prefix + currentLineContent);
+
+    const splitCmd = cmd.trim().split(' ');
+    switch (splitCmd[0]) {
+      case 'git':
+        currentLine += execGitCmd(term, splitCmd.length > 1 ? splitCmd[1] : 'help');
+        break;
+      case 'help':
+        const helpText = '\
+        \r\nWelcome to Git Sandbox! Try some of the commands below.\
+        \r\n\
+        \r\nhelp   Prints this help message\
+        \r\ngit    Simulate git commands\
+        \r\n\
+        \r\n';
+        term.write(helpText);
+        currentLine += incrementLines(helpText);
+        break;
+      case '':
+        const blankText = '\r\n\r\n';
+        term.write(blankText);
+        currentLine += incrementLines(blankText);
+        break;
+      default:
+        const notFoundText = `\
+        \r\n${splitCmd[0]}: command not found\
+        \r\n\
+        \r\n`;
+        term.write(notFoundText);
+        currentLine += incrementLines(notFoundText);
+    }
+
+    currentLineContent = '';
+    term.write(`${prefix}`);
+  };
+
+  const incrementLines = (text: string): number => {
+    let linesToAdd = 0;
+    const splitText = text.split('\r\n');
+    for (const lineText of splitText) {
+      const parsedLineText = lineText.replace(/[^\x00-\x7F]/g, '');
+      linesToAdd += Math.floor(parsedLineText.length / term.cols);
+    }
+    linesToAdd += splitText.length - 1;
+    return linesToAdd;
+  };
+
+  const toggleTerminal = () => {
+    setIsTerminalOpen(!isTerminalOpen);
+  };
+
+  return (
+    <div style={{ width: '900px', height: '600px' }}>
+      <div id='terminal' ref={terminalRef} style={{ width: 'inherit', height: 'inherit', display: `${isTerminalOpen ? 'block' : 'none'}` }} />
+      {/* <button onClick={() => toggleTerminal()}>Toggle Terminal</button> */}
+    </div>
+  );
+}
